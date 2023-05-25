ad828ce2f1a78d708aa89797019468fd232fb25f
Aditya Bawangade <105143252+Aditya-Bawangade@users.noreply.github.com>
2023-05-25 20:33:28 +0530

    An addition to this code

--- 1964. Find the Longest Valid Obstacle Course at Each Position.cpp
+++ 1964. Find the Longest Valid Obstacle Course at Each Position.cpp
@@ -1,24 +1,38 @@
 class Solution {
 public:
     vector<int> longestObstacleCourseAtEachPosition(vector<int>& obstacles) {
-        vector<int> ans, mono ;
-        for(int a : obstacles){
-            int l = 0, r = mono.size() ;
-            while(l < r){
-                int mid = (l+r)/2 ;
-                if(mono[mid] <= a){
-                    l = mid + 1 ; 
-                }
-                else{
-                    r = mid ;
-                } 
+        // vector<int> ans, mono ;
+        // for(int a : obstacles){
+        //     int l = 0, r = mono.size() ;
+        //     while(l < r){
+        //         int mid = (l+r)/2 ;
+        //         if(mono[mid] <= a){
+        //             l = mid + 1 ; 
+        //         }
+        //         else{
+        //             r = mid ;
+        //         } 
+        //     }
+        //     ans.push_back(l+1) ;
+        //     if(mono.size() == l){
+        //         mono.push_back(a) ;
+        //     }
+        //     mono[l] = a ;
+        // }
+        // return ans ;
+        vector<int> util ;
+        for(int i = 0 ; i < obstacles.size() ; i++){
+            int x = obstacles[i] ;
+            if(util.empty() || util[util.size()-1] <= x){
+                util.push_back(x) ;
+                obstacles[i] = util.size() ;
+            }else{
+                int index = upper_bound(util.begin(), util.end()
+                ,x) - util.begin() ;
+                util[index] = x ;
+                obstacles[i] = index + 1 ;
             }
-            ans.push_back(l+1) ;
-            if(mono.size() == l){
-                mono.push_back(a) ;
-            }
-            mono[l] = a ;
         }
-        return ans ;
+        return obstacles ;
     }
 };
