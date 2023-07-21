cbf96707242008b061d42470df362df72d3a0c78
Aditya Bawangade <105143252+Aditya-Bawangade@users.noreply.github.com>
2023-07-21 17:10:24 +0530

    Tough!!!

---
+++ CODENATION_Count distinct Bitwise OR of all subarrays.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h> 
+int countDistinctBitwiseOR(vector<int>& inputVector, int size)
+{
+	// unordered_set<int> st, prev, curr ;
+
+	// for(int i = 0 ; i < size ; i++){
+	// 	curr.insert(inputVector[i]) ;
+
+	// 	for(auto it : prev){
+	// 		curr.insert(inputVector[i] | it) ;
+	// 	}
+
+	// 	prev = curr ;
+	// 	curr.clear() ;
+	// 	for(auto it : prev){
+	// 		st.insert(it) ;
+	// 	}
+	// }
+	// return st.size() ;
+    vector<int> res ;
+    unordered_set<int> st ;
+    int left = 0 ; int right ;
+    for(int a : inputVector){
+        right = res.size() ;
+        res.push_back(a) ;
+        for(int i = left ; i < right ; i++){
+            if(res.back() != (res[i] | a)){
+                res.push_back(res[i] | a) ;
+            }
+        }
+        left = right ;
+    }
+
+    for(int i = 0 ; i < res.size() ; i++){
+        st.insert(res[i]) ;
+    }
+    return st.size() ;
+
+
+}
